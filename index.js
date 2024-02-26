6c1a451b9e4cdd36205b64eb01c6fabcd7980eb9
Gabriel <gabrielcostatoledo@outlook.com>
2024-02-26 14:43:38 -0300

    feat: add PostgresCreateUserRepository

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 import 'dotenv/config.js'
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(process.env.PORT, () => console.log('Listening on port 8080'))
