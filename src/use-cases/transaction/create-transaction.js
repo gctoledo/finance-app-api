51fbc14f86bc973f7d7b947399c58e220eb5bae6
Gabriel <gabrielcostatoledo@outlook.com>
2024-03-02 03:14:19 -0300

    feat: add CreateTransactionUseCase

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,32 @@
+import { UserNotFoundError } from '../../errors/user.js'
+import { v4 as uuidv4 } from 'uuid'
+
+export class CreateTransactionUseCase {
+    constructor(postgresCreateTransactionRepository, getUserByIdRepository) {
+        ;(this.postgresCreateTransactionRepository =
+            postgresCreateTransactionRepository),
+            (this.getUserByIdRepository = getUserByIdRepository)
+    }
+    async execute(createTransactionParams) {
+        //VALIDAR SE O USUARIO EXISTE
+
+        const userId = createTransactionParams.user_id
+
+        const user = await this.getUserByIdRepository.execute(userId)
+
+        if (!user) {
+            throw new UserNotFoundError(userId)
+        }
+
+        // GERA ID DA TRANSACAO
+        const transactionId = uuidv4()
+
+        const transaction =
+            await this.postgresCreateTransactionRepository.execute({
+                ...createTransactionParams,
+                id: transactionId,
+            })
+
+        return transaction
+    }
+}
