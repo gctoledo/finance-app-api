e7d3c2a9226a2196037d711b5a3558bb708bf8f8
Gabriel <gabrielcostatoledo@outlook.com>
2024-03-11 13:29:57 -0300

    refactor: move user controllers to its own folder

--- src/controllers/user/get-user-by-id.js
+++ src/controllers/user/get-user-by-id.js
@@ -4,7 +4,7 @@ import {
     userNotFoundResponse,
     invalidIdResponse,
     checkIfIdIsValid,
-} from './helpers/index.js'
+} from '../helpers/index.js'
 
 export class GetUserByIdController {
     constructor(getUserByIdUseCase) {
