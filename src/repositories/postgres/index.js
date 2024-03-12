73453faf8a0f553e30c03d93b71a324cf2b74b0e
Gabriel <gabrielcostatoledo@outlook.com>
2024-03-12 14:06:29 -0300

    feat: add CreateTransactionController factory

--- src/repositories/postgres/index.js
+++ src/repositories/postgres/index.js
@@ -3,4 +3,5 @@ export * from './user/get-user-by-email.js'
 export * from './user/get-user-by-id.js'
 export * from './user/update-user.js'
 export * from './user/delete-user.js'
-1
+
+export * from './transaction/create-transaction.js'
