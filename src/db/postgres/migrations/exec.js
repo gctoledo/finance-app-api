d4b2277834d74caf4abe134609d083006c5a5872
Gabriel <gabrielcostatoledo@outlook.com>
2024-04-17 15:18:09 -0300

    feat: update execMigrations script to read all sql files

--- src/db/postgres/migrations/exec.js
+++ src/db/postgres/migrations/exec.js
@@ -11,13 +11,21 @@ const execMigrations = async () => {
     const client = await pool.connect()
 
     try {
-        const filePath = path.join(__dirname, '01-init.sql')
+        const files = fs
+            .readdirSync(__dirname)
+            .filter((file) => file.endsWith('.sql'))
 
-        const script = fs.readFileSync(filePath, 'utf-8')
+        for (const file of files) {
+            const filePath = path.join(__dirname, file)
 
-        await client.query(script)
+            const script = fs.readFileSync(filePath, 'utf-8')
 
-        console.log('Migrations executed successfully.')
+            await client.query(script)
+
+            console.log(`Migration for file ${file} executed successfully`)
+        }
+
+        console.log('All migrations were executed successfully.')
     } catch (err) {
         console.error(err)
     } finally {
