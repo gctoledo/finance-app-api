9d7e26a07ce51ea8d5fd55f29e3331c33b966954
Gabriel <gabrielcostatoledo@outlook.com>
2024-04-29 14:15:27 -0300

    test: ensure CreateUserUseCase is called with correct params

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -23,7 +23,7 @@ describe('Create User Controller', () => {
         const result = await createUserController.execute(httpRequest)
 
         expect(result.statusCode).toBe(201)
-        expect(result.body).toBe(httpRequest.body)
+        expect(result.body).toEqual(httpRequest.body)
     })
 
     it('should return 400 if first_name is not provided', async () => {
@@ -146,5 +146,28 @@ describe('Create User Controller', () => {
 
         // assert
         expect(result.statusCode).toBe(400)
+    })
+
+    it('should call CreateUserUseCase with correct params', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Gabriel',
+                last_name: 'Toledo',
+                email: 'ga@toledo.com',
+                password: '123',
+            },
+        }
+
+        const executeSpy = jest.spyOn(createUserUseCase, 'execute')
+
+        // act
+        await createUserController.execute(httpRequest)
+
+        // assert
+        expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
     })
 })
