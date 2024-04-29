ddf09ccdf2d94baa7a047ad1f6d1f775311d72b0
Gabriel <gabrielcostatoledo@outlook.com>
2024-04-29 13:41:41 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -28,13 +28,32 @@ describe('Create User Controller', () => {
 
     it('should return 400 if first_name is not provided', async () => {
         // arrange
-
         const createUserUseCase = new CreateUserUseCaseStub()
         const createUserController = new CreateUserController(createUserUseCase)
 
         const httpRequest = {
             body: {
                 last_name: 'Toledo',
+                email: 'ga@toledo.com',
+                password: '1234567',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 if last_name is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Gabriel',
                 email: 'ga@toledo.com',
                 password: '1234567',
             },
