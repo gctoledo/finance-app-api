e91f690c616c513bd29aa263e083418a4bb74f7a
Gabriel <gabrielcostatoledo@outlook.com>
2024-04-29 19:23:43 -0300

    test: ensure DeleteUserController returns 200 on user deletion

---
+++ src/controllers/user/delete-user.test.js
@@ -0,0 +1,40 @@
+import { DeleteUserController } from './delete-user.js'
+import { faker } from '@faker-js/faker'
+
+describe('DeleteUserController', () => {
+    class DeleteUserUseCaseStub {
+        execute() {
+            return {
+                id: faker.string.uuid(),
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 7 }),
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const deleteUserUseCase = new DeleteUserUseCaseStub()
+        const deleteUserController = new DeleteUserController(deleteUserUseCase)
+
+        return { deleteUserUseCase, deleteUserController }
+    }
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    }
+
+    it('should return 200 if user is deleted', async () => {
+        // arrange
+        const { deleteUserController } = makeSut()
+
+        // act
+        const result = await deleteUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(200)
+    })
+})
