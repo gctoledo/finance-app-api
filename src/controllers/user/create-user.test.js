408513174b8e1718328a7f6fc7585530e7098eff
Gabriel <gabrielcostatoledo@outlook.com>
2024-04-29 13:59:10 -0300

    test: ensure 400 is returned if password is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -126,4 +126,25 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if password is less then 6 characters', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'Gabriel',
+                last_name: 'Toledo',
+                email: 'ga@toledo.com',
+                password: '123',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
