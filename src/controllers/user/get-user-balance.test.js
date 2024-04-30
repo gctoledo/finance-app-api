54d961819df50cfa52efcdec74c1bdbfe6716435
Gabriel <gabrielcostatoledo@outlook.com>
2024-04-30 12:54:55 -0300

    test: ensure GetUserBalanceController returns 200 when getting user balance

---
+++ src/controllers/user/get-user-balance.test.js
@@ -0,0 +1,36 @@
+import { faker } from '@faker-js/faker'
+import { GetUserBalanceController } from './get-user-balance.js'
+
+describe('GetUserBalanceController', () => {
+    class GetUserBalanceUseCaseStub {
+        execute() {
+            return faker.number.int()
+        }
+    }
+
+    const makeSut = () => {
+        const getUserBalanceUseCase = new GetUserBalanceUseCaseStub()
+        const getUserBalanceController = new GetUserBalanceController(
+            getUserBalanceUseCase,
+        )
+
+        return { getUserBalanceController, getUserBalanceUseCase }
+    }
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    }
+
+    it('should return 200 when getting user balance', async () => {
+        //arrange
+        const { getUserBalanceController } = makeSut()
+
+        //act
+        const result = await getUserBalanceController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(200)
+    })
+})
