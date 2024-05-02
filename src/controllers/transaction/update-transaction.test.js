9cb4b0dabaed145b56615ebae9c8bb656421e0cc
Gabriel <gabrielcostatoledo@outlook.com>
2024-05-02 16:48:24 -0300

    test: ensure UpdateTransactionController returns 200 when updating transaction

---
+++ src/controllers/transaction/update-transaction.test.js
@@ -0,0 +1,49 @@
+import { UpdateTransactionController } from './update-transaction.js'
+import { faker } from '@faker-js/faker'
+
+describe('UpdateTransactionController', () => {
+    class UpdateTransactionUseCaseStub {
+        async execute() {
+            return {
+                user_id: faker.string.uuid(),
+                id: faker.string.uuid(),
+                name: faker.string.alphanumeric(10),
+                date: faker.date.anytime().toISOString(),
+                type: 'EXPENSE',
+                amount: Number(faker.finance.amount()),
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const updateTransactionUseCase = new UpdateTransactionUseCaseStub()
+        const updateTransactionController = new UpdateTransactionController(
+            updateTransactionUseCase,
+        )
+
+        return { updateTransactionController, updateTransactionUseCase }
+    }
+
+    const httpRequest = {
+        params: {
+            transactionId: faker.string.uuid(),
+        },
+        body: {
+            name: faker.string.alphanumeric(10),
+            date: faker.date.anytime().toISOString(),
+            type: 'EXPENSE',
+            amount: Number(faker.finance.amount()),
+        },
+    }
+
+    it('should return 200 when updating transaction', async () => {
+        //arrange
+        const { updateTransactionController } = makeSut()
+
+        //act
+        const result = await updateTransactionController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(200)
+    })
+})
