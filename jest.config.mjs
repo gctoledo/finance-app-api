9b999fb9cb6fb05475f530b8e1948a0c4ed34de5
Gabriel <gabrielcostatoledo@outlook.com>
2024-05-03 16:14:12 -0300

    chore: add jest coverage setup

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,6 +8,7 @@ const config = {
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
     watchPathIgnorePatterns: ['/node_modules/', '.postgres-data'],
+    collectCoverageFrom: ['src/**/*.js'],
 }
 
 export default config
