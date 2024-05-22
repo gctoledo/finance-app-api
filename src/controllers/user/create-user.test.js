a1ca9138b55f6b787ee2427e193fb7c3bdd37f74
Gabriel <gabrielcostatoledo@outlook.com>
2024-05-22 14:09:51 -0300

    test: ensure CreateUserController use fixtures on stubs

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -1,10 +1,11 @@
 import { EmailAlreadyInUseError } from '../../errors/user.js'
 import { CreateUserController } from './create-user.js'
 import { faker } from '@faker-js/faker'
+import { user } from '../../tests/index.js'
 
-describe('Create User Controller', () => {
+describe('CreateUserController', () => {
     class CreateUserUseCaseStub {
-        async execute(user) {
+        async execute() {
             return user
         }
     }
@@ -18,10 +19,8 @@ describe('Create User Controller', () => {
 
     const httpRequest = {
         body: {
-            first_name: faker.person.firstName(),
-            last_name: faker.person.lastName(),
-            email: faker.internet.email(),
-            password: faker.internet.password({ length: 7 }),
+            ...user,
+            id: undefined,
         },
     }
 
@@ -34,7 +33,7 @@ describe('Create User Controller', () => {
 
         // assert
         expect(result.statusCode).toBe(201)
-        expect(result.body).toEqual(httpRequest.body)
+        expect(result.body).toEqual(user)
     })
 
     it('should return 400 if first_name is not provided', async () => {
